25b515fd4d4a2c4fbbdbb412a62a83720c9524a1
bingbingbog <libingbingwork@gmail.com>
2020-10-29 10:53:22 +0800

    Update EasyContactPickerPlugin.java

--- android/src/main/java/com/liuwei/easy_contact_picker/EasyContactPickerPlugin.java
+++ android/src/main/java/com/liuwei/easy_contact_picker/EasyContactPickerPlugin.java
@@ -12,6 +12,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -79,11 +101,11 @@ public class EasyContactPickerPlugin implements MethodCallHandler, PluginRegistr
       };
       getContacts();
     }
-     else if (call.method.equals(CallHistoryList)){
+    else if (call.method.equals(CallHistoryList)){
       contactsCallBack = new ContactsCallBack() {
         @Override
-        void successWithList(List<HashMap> contacts) {
-          super.successWithList(contacts);
+        void successWithLists(List<HashMap> contacts) {
+          super.successWithLists(contacts);
           result.success(contacts);
         }
 
@@ -92,7 +114,7 @@ public class EasyContactPickerPlugin implements MethodCallHandler, PluginRegistr
           super.error();
         }
       };
-      getContacts();
+      getCallHistoryList(mActivity);
     }
   }
 
@@ -142,6 +164,105 @@ public class EasyContactPickerPlugin implements MethodCallHandler, PluginRegistr
 
   }
 
+  @TargetApi(Build.VERSION_CODES.N)
+  @SuppressLint("MissingPermission")
+  private void getCallHistoryList(Activity activity){
+    List contacts = new ArrayList<>();
+    Cursor cs;
+    cs = activity.getContentResolver().query(CallLog.Calls.CONTENT_URI, //系统方式获取通讯录存储地址
+            new String[]{
+                    CallLog.Calls.CACHED_NAME,  //姓名
+                    CallLog.Calls.NUMBER,    //号码
+                    CallLog.Calls.TYPE,  //呼入/呼出(2)/未接
+                    CallLog.Calls.DATE,  //拨打时间
+                    CallLog.Calls.DURATION,   //通话时长
+            }, null, null, CallLog.Calls.DEFAULT_SORT_ORDER);
+    int i = 0;
+    if (cs != null && cs.getCount() > 0) {
+      Date date = new Date(System.currentTimeMillis());
+      SimpleDateFormat simpleDateFormat = null;
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+      }
+      String date_today = simpleDateFormat.format(date);
+      for (cs.moveToFirst(); (!cs.isAfterLast()) && i < 100; cs.moveToNext(), i++) {
+        HashMap<String, String> map =  new HashMap<String, String>();
+        String callName = cs.getString(0);  //名称
+        String callNumber = cs.getString(1);  //号码
+        //如果名字为空，在通讯录查询一次有没有对应联系人
+        if (callName == null || callName.equals("")){
+          String[] cols = {ContactsContract.PhoneLookup.DISPLAY_NAME};
+          //设置查询条件
+          String selection = ContactsContract.CommonDataKinds.Phone.NUMBER + "='"+callNumber+"'";
+          Cursor cursor = activity.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                  cols, selection, null, null);
+          int nameFieldColumnIndex = cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
+          if (cursor.getCount()>0){
+            cursor.moveToFirst();
+            callName = cursor.getString(nameFieldColumnIndex);
+          }
+          cursor.close();
+        }
+
+        //拨打时间
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date callDate = new Date(Long.parseLong(cs.getString(3)));
+        String callDateStr = sdf.format(callDate);
+        if (callDateStr.equals(date_today)) { //判断是否为今天
+          sdf = new SimpleDateFormat("HH:mm");
+          callDateStr = sdf.format(callDate);
+        } else if (date_today.contains(callDateStr.substring(0, 7))) { //判断是否为当月
+          sdf = new SimpleDateFormat("dd");
+          int callDay = Integer.valueOf(sdf.format(callDate));
+
+          int day = Integer.valueOf(sdf.format(date));
+          if (day - callDay == 1) {
+            callDateStr = "昨天";
+          } else {
+            sdf = new SimpleDateFormat("MM-dd");
+            callDateStr = sdf.format(callDate);
+          }
+        } else if (date_today.contains(callDateStr.substring(0, 4))) { //判断是否为当年
+          sdf = new SimpleDateFormat("MM-dd");
+          callDateStr = sdf.format(callDate);
+        }
+
+        //通话时长
+        int callDuration = Integer.parseInt(cs.getString(4));
+        int min = callDuration / 60;
+        int sec = callDuration % 60;
+        String callDurationStr = "";
+        if (sec > 0) {
+          if (min > 0) {
+            callDurationStr = min + "分" + sec + "秒";
+          } else {
+            callDurationStr = sec + "秒";
+          }
+        }
+
+        /**
+         * callName 名字
+         * callNumber 号码
+         * callTypeStr 通话类型
+         * callDateStr 通话日期
+         * callDurationStr 通话时长
+         * 请在此处执行相关UI或存储操作，之后会查询下一条通话记录
+         */
+        Log.i("Msg","callName"+callName);
+        Log.i("Msg","callNumber"+callNumber);
+        Log.i("Msg","callDateStr"+callDateStr);
+        Log.i("Msg","callDurationStr"+callDurationStr);
+        map.put("callName", callName);
+        map.put("callNumber", callNumber);
+        map.put("callDateStr", callDateStr);
+        map.put("callDurationStr", callDurationStr);
+        contacts.add(map);
+      }
+      contactsCallBack.successWithLists(contacts);
+    }
+
+  }
+
   @Override
   public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
     if(requestCode==0x30) {
@@ -179,7 +300,9 @@ public class EasyContactPickerPlugin implements MethodCallHandler, PluginRegistr
   /** 获取通讯录回调. */
   public abstract class ContactsCallBack{
     void successWithList(List<HashMap> contacts){};
+    void successWithLists(List<HashMap> contacts){};
     void successWithMap(HashMap<String, String> map){};
     void error(){};
   }
 }
+
